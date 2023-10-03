cb1f877caa4a08e7504b26f0b0a43d573e58fd79
ryuken73 <ryuken73@gmail.com>
2023-10-03 23:40:20 +0900

    update: throttle fires latest value too

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,19 @@
 export const throttle = (callbackFn, limit) => {
   let wait = false;                  
+  let timer;
   return function (args) {              
-      if (!wait) {                  
-          callbackFn.call(this, args);           
-          wait = true;               
-          setTimeout(function () {   
-              wait = false;          
-          }, limit);
-      }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callbackFn.call(this, args);           
+      clearTimeout(timer);
+    }, limit);
+    if (!wait) {                  
+        callbackFn.call(this, args);           
+        wait = true;               
+        clearTimeout(timer);
+        setTimeout(function () {   
+            wait = false;          
+        }, limit);
+    }
   }
 }
