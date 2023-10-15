4d8dad1be466bad3bf906d5cfccda8e794635525
ryuken73 <ryuken73@gmail.com>
2023-10-15 16:23:53 +0900

    update: slider width is now responsive

--- src/Components/SlidingRadio/index.js
+++ src/Components/SlidingRadio/index.js
@@ -12,7 +12,8 @@ const Container = styled.div`
   border-radius: 8px;
   color: white;
   box-sizing: border-box;
-  width: fit-content;
+  /* width: fit-content; */
+  width: 100%;
   height: 2rem;
   margin: 0 auto;
   font-size: 0.8rem;
@@ -22,7 +23,8 @@ const Item = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 4rem;
+  /* width: 4rem; */
+  width: 100%;
   height: 1.5rem;
   text-align: center;
   box-sizing: border-box;
@@ -34,7 +36,7 @@ const Sliding = styled.div`
   z-index: 1;
   background-color: darkslategray;
   height: 1.5rem;
-  width: 4rem;
+  width: ${props => `calc(${props.sliderWidth}px - 0.1rem)`};
   border-radius: 5px;
   box-sizing: border-box;
   transition: 0.25s ease-out;
@@ -50,7 +52,27 @@ const itemList = {
 
 function SlidingRadio() {
   const {globalPeriod, setGlobalPeriodState} = useAppState();
+  const [sliderWidth, setSliderWidth] = React.useState('0px');
+  const containerRef = React.useRef(null);
   const slideRef = React.useRef(null);
+
+
+  const resizeSlider = React.useCallback(() => {
+    if(containerRef.current === null) return;
+    const clientRect = containerRef.current.getBoundingClientRect();
+    setSliderWidth(clientRect.width / 5);
+  }, [])
+
+  React.useEffect(() => {
+    resizeSlider();
+  }, [resizeSlider])
+
+  React.useEffect(() => {
+    window.addEventListener('resize', resizeSlider)
+    return () => {
+      window.removeEventListener('resize', resizeSlider);
+    }
+  }, [resizeSlider])
 
   React.useEffect(() => {
     const index = Object.keys(itemList).findIndex(period => period === globalPeriod);
@@ -62,12 +84,12 @@ function SlidingRadio() {
     const index = event.target.id;
     if(typeof(index) === 'undefined' || index.length === 0) return;
     const currentPeriod = Object.keys(itemList)[index];
-    slideRef.current.style.transform = `translateX(${index * 100}%)`;
+    slideRef.current.style.trancontainerRefsform = `translateX(${index * 100}%)`;
     setGlobalPeriodState(currentPeriod);
   }, [setGlobalPeriodState])
 
   return (
-    <Container onClick={onClickItem}>
+    <Container ref={containerRef} onClick={onClickItem}>
       {Object.keys(itemList).map((key, index) => (
         <Item key={key} id={index}>
           <div id={index}>
@@ -75,7 +97,7 @@ function SlidingRadio() {
           </div>
         </Item>
       ))}
-      <Sliding ref={slideRef}></Sliding>
+      <Sliding ref={slideRef} sliderWidth={sliderWidth}></Sliding>
     </Container>
   )
 }
